63ac27b22701f1a3b81e6a9e68a866a1c6064b89
Savonarola Girolamo <savonarolagirolamo@yandex.ru>
2023-09-30 04:08:39 +0300

    refactor: Change receivers order in config

--- config/index.ts
+++ config/index.ts
@@ -23,6 +23,18 @@ export const config = {
     data: {
       owner: "0:89163cee6448507a8aa66c8452a31d5f0d774b92da0ed07cf7a118de9b06f8cb",
       receivers: [
+        // Development fund - 78%
+        {
+          wallet: '0:d834d6e2a26717e8f5f735d614d80059c34723702096754173cfc25e189e28e0',
+          share: 78e7
+        },
+
+        // Promotion fund - 10%
+        {
+          wallet: '0:5210528bc9f67745d9ef3fc5b3038342c1e755ca5d38f550f991ecd0400694a7',
+          share: 10e7
+        },
+
         // Charitable fund - 10%
         {
           wallet: '0:d609c105cb87ea4e78341968e86d326c8daebbfd03629dcc0c5c098206349224',
@@ -35,23 +47,11 @@ export const config = {
           share: 1e7
         },
 
-        // Promotion fund - 10%
-        {
-          wallet: '0:5210528bc9f67745d9ef3fc5b3038342c1e755ca5d38f550f991ecd0400694a7',
-          share: 10e7
-        },
-
         // Technical Support - 1%
         {
           wallet: '0:66d638126db3157f585013c105fa168a1544b41bc8f25538bac38b3ac82c13fc',
           share: 1e7
         },
-
-        // Development fund - 78%
-        {
-          wallet: '0:d834d6e2a26717e8f5f735d614d80059c34723702096754173cfc25e189e28e0',
-          share: 78e7
-        }
       ],
       minDeposit: B,
       coinsTransferValue: 0.02 * B,
